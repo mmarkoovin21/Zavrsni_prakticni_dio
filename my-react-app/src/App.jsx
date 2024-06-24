493477273a11a2dcc41f03b9108fbc5686dc7994
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-06-24 12:08:58 +0200

    Made initial version of header component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Footer from './Components/footer'
+import Header from './Components/header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>ovo radi?</h1>
       <Footer />
       </div>
---
+++ my-react-app/src/Components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <h2>Course Manager</h2>
+    </header>
+  );
+}
+
+export default Header;
