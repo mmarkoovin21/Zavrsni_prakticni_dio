a9cea0f63a0cff7043eaa06bd76dc9d552f3a873
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-06-26 22:43:09 +0200

    Made login logic that navigates to home page

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,21 @@
-import './App.css'
-import Footer from './Components/footer'
-import Login from './Components/login'
+import './App.css';
+import Footer from './Components/footer';
+import Login from './Components/login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Pocetna from './Components/Pocetna';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-      <Footer />
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/pocetna" element={<Pocetna />} />
+        </Routes>
+        <Footer />
       </div>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- my-react-app/src/Components/login.jsx
+++ my-react-app/src/Components/login.jsx
@@ -1,18 +1,48 @@
+import React, {useState} from 'react';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
 import '@fontsource/roboto/400.css';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
+import {Link, useNavigate } from 'react-router-dom';
 
 function Login () {
+    const navigate = useNavigate();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        if (username === 'mmarkovin21' && password === 'password123') {	
+            navigate('/pocetna');
+        } else {
+            alert('Pogrešno korisničko ime ili lozinka');
+        }
+    }
+
   return (
     <div className='login-container'>
-      <form>
+      <form onSubmit={handleLogin}>
         <Paper className='login-text-fields' elevation={3}>
             <Typography variant="h2" id="welcome-title" sx={{ width: '60%', mt: '3rem'}}>Dobrodošli u Course Manager</Typography>
-            <TextField id="standard-basic" label="Korisničko ime" variant="standard" sx={{ width: '50%', mt: '1rem'}} />
-            <TextField id="standard-basic" label="Lozinka" variant="standard" sx={{ width: '50%' }}/>
-            <Button variant="contained" size="large">Prijavi se</Button>
+            <TextField 
+            id="standard-basic" 
+            label="Korisničko ime" 
+            variant="standard" 
+            sx={{ width: '50%', mt: '1rem'}}
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)} />
+
+            <TextField 
+            id="standard-basic" 
+            label="Lozinka" 
+            variant="standard" 
+             type="password"
+            sx={{ width: '50%' }}
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)}/>
+
+            <Button type='submit' variant="contained" size="large">Prijavi se</Button>
+            <Link to="/pocetna">početna</Link>
         </Paper>
       </form>
     </div>
