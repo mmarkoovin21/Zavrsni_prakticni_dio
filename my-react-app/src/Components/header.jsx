3488af50189794150cdeab44741ced26d7cce342
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-06-27 20:46:29 +0200

    Edited Header so it looks like one in FIgma

--- my-react-app/src/Components/header.jsx
+++ my-react-app/src/Components/header.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import avatarImage from '../assets/1650496476404.jpg';
 
 function Header() {
   return (
     <header className="header">
-      <h2>Course Manager</h2>
+      <MenuIcon sx={{ fontSize: 45 }}/>
+      <Typography variant="h4" sx={{ width: '99%'}}>
+        Course Manager
+      </Typography>
+      <Avatar alt="mmarkovin21" src={avatarImage} sx={{ width: 45, height: 45 }}/>
     </header>
   );
 }
