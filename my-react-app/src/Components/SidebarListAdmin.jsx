9bceba71007a30cafde20602a09fdfd5ed26ddcc
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-06-28 21:08:29 +0200

    Made Katalozi dropdown on Sidebar

--- my-react-app/src/Components/SidebarListAdmin.jsx
+++ my-react-app/src/Components/SidebarListAdmin.jsx
@@ -1,32 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ArrowForwardIos from '@mui/icons-material/ArrowForwardIos';
-import Divider from '@mui/material/Divider';
+import Collapse from '@mui/material/Collapse';
 import { Link } from 'react-router-dom';
+import { Divider } from '@mui/material';
 
 function SidebarListAdmin() {
+  const [open, setOpen] = useState(false);
+  const [rotate, setRotate] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+    setRotate(!rotate);
+  };
+
   const items = [
     { name: 'Početna', path: '/pocetna-admin' },
     { name: 'Akademska Godina', path: '/ak-godina' },
-    { name: 'Katalozi', path: '/pocetna-admin' },
     { name: 'Dnevnik aktivnosti', path: '/pocetna-admin' },
+  ];
+
+  const dropdownItems = [
+    { name: 'Akademske godine', path: '/akademske-godine' },
+    { name: 'Predmeti', path: '/predmeti' },
+    { name: 'Nastavnici', path: '/nastavnici' },
   ];
 
   return (
     <List>
-      {items.map((item, index) => (
-        <React.Fragment key={item.name}>
-          <ListItem component={Link} to={item.path}>
-            <ListItemText primary={item.name} sx={{ mr: 2 }}/>
-            <ListItemIcon >
-              <ArrowForwardIos />
-            </ListItemIcon>
-          </ListItem>
-          {index === items.length - 2  && <Divider />}
-        </React.Fragment>
+      {items.filter(item => item.name !== 'Dnevnik aktivnosti').map((item) => (
+        <ListItem key={item.name} component={Link} to={item.path}>
+          <ListItemText primary={item.name} />
+        </ListItem>
+      ))}
+      {/* Katalozi and its dropdown */}
+      <ListItem button onClick={handleClick}>
+        <ListItemText primary="Katalozi" />
+        <ListItemIcon style={{ transform: rotate ? 'rotate(90deg)' : 'none' }}>
+          <ArrowForwardIos />
+        </ListItemIcon>
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          {dropdownItems.map((dropdownItem) => (
+            <ListItem key={dropdownItem.name} component={Link} to={dropdownItem.path} sx={{ pl: 4 }}>
+              <ListItemText primary={dropdownItem.name} />
+            </ListItem>
+          ))}
+        </List>
+      </Collapse>
+      <Divider />
+      {/* Dnevnik aktivnosti */}
+      {items.filter(item => item.name === 'Dnevnik aktivnosti').map((item) => (
+        <ListItem key={item.name} component={Link} to={item.path}>
+          <ListItemText primary={item.name} />
+        </ListItem>
       ))}
     </List>
   );
