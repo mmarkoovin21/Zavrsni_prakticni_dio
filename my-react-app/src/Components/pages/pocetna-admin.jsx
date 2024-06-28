efef0ba533ce27949b53aa6a3db21cd54c4b9e7d
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-06-28 23:44:46 +0200

    Made most of components for admin home screen.
    Statistic cards, top navigation links and title were made.

--- my-react-app/src/Components/pages/pocetna-admin.jsx
+++ my-react-app/src/Components/pages/pocetna-admin.jsx
@@ -1,6 +1,11 @@
 import Header from "../header";
 import React, { useState } from "react";
 import PersistentSidebar from "../PersistentSidebar";
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { ArrowForwardIos } from "@mui/icons-material";
+import Statistics from "../Statistics";
+
 
 function PocetnaAdmin() {
   const [open, setOpen] = useState(false);
@@ -13,6 +18,17 @@ function PocetnaAdmin() {
     <div className="App">
       <Header toggleDrawer={toggleDrawer} />
       <PersistentSidebar open={open} handleDrawerClose={toggleDrawer} />
+      <main>
+        <div className="topNavigation">
+          <Link to="/pocetna-admin">Administrator</Link>
+          <ArrowForwardIos sx={{height: '14px'}}/>
+          <Link to="/pocetna-admin">Početna</Link>
+        </div>
+        <Typography variant="h3" sx={{marginBottom: '20px'}}>
+          Statistika
+        </Typography>
+        <Statistics />
+      </main>
     </div>
   );
 }
