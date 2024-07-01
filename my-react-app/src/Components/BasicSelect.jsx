f065f8bcaedeec1f26884ecbaf8e43cacd5d16f7
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-07-01 13:49:33 +0200

    made now component BasicSelect and edited page 'ak-godine'

---
+++ my-react-app/src/Components/BasicSelect.jsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+function BasicSelect({label, dropdownOptions}){
+  const [selectedValue, setValue] = React.useState('');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">{label}</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={selectedValue}
+          label={label}
+          onChange={handleChange}
+        >
+          {Object.entries(dropdownOptions).map(([value, label]) => (
+            <MenuItem key={value} value={value}>{label}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
+
+export default BasicSelect;
--- my-react-app/src/Components/pages/ak-godina.jsx
+++ my-react-app/src/Components/pages/ak-godina.jsx
@@ -1,7 +1,37 @@
+import { Link } from 'react-router-dom';
+import { ArrowForwardIos } from "@mui/icons-material";
+import { Typography } from '@mui/material';
+import BasicSelect from '../BasicSelect';
+import TextField from '@mui/material/TextField';
+
+let dropdownOptions = {
+  2020: '2020',
+  2021: '2021',
+  2022: '2022',
+  2023: '2023',
+  2024: '2024',
+};
+
 function AkGodina() {
+  console.log('Rendering AkGodina');
   return (
     <div>
-      <h1>Ak Godina</h1>
+      <main>
+      <div className="topNavigation">
+          <Link to="/pocetna-admin">Administrator</Link>
+          <ArrowForwardIos sx={{height: '14px'}}/>
+          <Link to="/pocetna-admin">Katalozi</Link>
+          <ArrowForwardIos sx={{height: '14px'}}/>
+          <Link to="/ak-godine">Akademske godine</Link>
+        </div>
+        <Typography variant="h3" sx={{marginBottom: '20px'}}>
+          Dostupni kolegiji u akademskoj godini
+        </Typography>
+        <div className='searchFilter'>
+          <BasicSelect label="Godina" dropdownOptions={dropdownOptions}/>
+          <TextField  />
+        </div>
+      </main>
     </div>
   );
 }
