1bef9bbcbecdf5d83bfb9aa5cd901e2bf83d1a99
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-07-02 12:33:28 +0200

    Refactored Statistics component for reuse on diferent pages.
    Now only one card is rendered with its children and its image as argument.

--- my-react-app/src/Components/Statistics.jsx
+++ my-react-app/src/Components/Statistics.jsx
@@ -1,33 +1,16 @@
 import { Typography } from "@mui/material";
 import Paper from '@mui/material/Paper';
-import statPng1 from '../assets/statistika 1.png';
-import statPng2 from '../assets/statistika 2.png';
 
-function Statistics (){
+function Statistics ({image, children}){
     return (
-      <div className="statistics">
-      <Paper elevation={2} sx={{ width: '99%', ml: '1.2rem', backgroundColor: '#B3E5FC', py: '30px' }}>
+      <Paper elevation={2} sx={{ width: '80%', backgroundColor: '#B3E5FC', py: '30px', ml: '25px' }}>
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
           <div className="statisticsText">
-            <Typography variant="h5" sx={{ mb: '15px' }}>Broj aktivnih kolegija: 48 </Typography>
-            <Typography variant="h5" sx={{ mb: '15px' }}>Ukupni broj kolegija: 72 </Typography>
-            <Typography variant="h5" sx={{ mb: '15px' }}>Novi kolegiji ove godine: 6</Typography>
+            {children}
           </div>
-          <img src={statPng1} alt="stat1" style={{ marginRight: '70px' }} />
+          <img src={image} alt="stats" style={{ marginRight: '70px' }} />
         </div>
       </Paper>
-      <Paper elevation={2} sx={{ width: '99%', ml: '1.2rem', backgroundColor: '#B3E5FC', py: '30px'  }}>
-        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <div className="statisticsText">
-            <Typography variant="h5" sx={{mb: '15px'}}>Ukupno profesora: 84 </Typography>
-            <Typography variant="h5" sx={{mb: '15px'}}>Ukupno studenata: 3754</Typography>
-            <Typography variant="h5" sx={{mb: '15px'}}>Profesori na čekanju: 2</Typography>
-            <Typography variant="h5" sx={{mb: '15px'}}>Studenti na čekanju: 21</Typography>
-            </div>
-          <img src={statPng2} alt="stat2" style={{ marginRight: '70px' }} />
-        </div>
-      </Paper>
-    </div>
     )
 }
 
--- my-react-app/src/Components/pages/pocetna-admin.jsx
+++ my-react-app/src/Components/pages/pocetna-admin.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import { ArrowForwardIos } from "@mui/icons-material";
 import Statistics from "../Statistics";
 import RecentCard from "../RecentCard";
+import statPng1 from '../../assets/statistika 1.png';
+import statPng2 from '../../assets/statistika 2.png';
 
 function PocetnaAdmin() {
   return (
@@ -17,7 +19,19 @@ function PocetnaAdmin() {
         <Typography variant="h3" sx={{marginBottom: '20px'}}>
           Statistika
         </Typography>
-        <Statistics />
+        <div className="statistics">
+          <Statistics image={statPng1}>
+          <Typography variant="h5" sx={{ mb: '15px' }}>Broj aktivnih kolegija: 48 </Typography>
+            <Typography variant="h5" sx={{ mb: '15px' }}>Ukupni broj kolegija: 72 </Typography>
+            <Typography variant="h5" sx={{ mb: '15px' }}>Novi kolegiji ove godine: 6</Typography>
+          </Statistics>
+          <Statistics image={statPng2}>
+            <Typography variant="h5" sx={{mb: '15px'}}>Ukupno profesora: 84 </Typography>
+            <Typography variant="h5" sx={{mb: '15px'}}>Ukupno studenata: 3754</Typography>
+            <Typography variant="h5" sx={{mb: '15px'}}>Profesori na čekanju: 2</Typography>
+            <Typography variant="h5" sx={{mb: '15px'}}>Studenti na čekanju: 21</Typography>
+          </Statistics>
+        </div>
         <Typography variant="h3" sx={{marginBottom: '20px'}}>
           Nedavno
         </Typography>
